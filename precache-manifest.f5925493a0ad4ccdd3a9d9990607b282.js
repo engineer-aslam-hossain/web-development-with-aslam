5bec190f0094ebf50107f9548ea599516c12d21a
unknown <engineer.aslamhossain@gmail.com>
2020-08-27 10:49:19 +0600

    Updates

---
+++ precache-manifest.f5925493a0ad4ccdd3a9d9990607b282.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "566d2b3ff94ee793cba7bae286bc544d",
+    "url": "/web-development-with-aslam/index.html"
+  },
+  {
+    "revision": "aba39d464480a843a842",
+    "url": "/web-development-with-aslam/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "81dc3d9582bd71918027",
+    "url": "/web-development-with-aslam/static/css/main.0e06d57f.chunk.css"
+  },
+  {
+    "revision": "aba39d464480a843a842",
+    "url": "/web-development-with-aslam/static/js/2.e1e62eb2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/web-development-with-aslam/static/js/2.e1e62eb2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81dc3d9582bd71918027",
+    "url": "/web-development-with-aslam/static/js/main.fe37b5bc.chunk.js"
+  },
+  {
+    "revision": "85c017848a32c72f9339",
+    "url": "/web-development-with-aslam/static/js/runtime-main.4596e68b.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/web-development-with-aslam/precache-manifest.f5925493a0ad4ccdd3a9d9990607b282.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/web-development-with-aslam/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
